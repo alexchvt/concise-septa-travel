12cf9c6e530bbacf305baff2862335e2a50731ad
Alex Chavet <alex.chavet@papernest.com>
2021-10-18 17:18:46 +0200

    First commit

---
+++ src/views/Players.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <PlayersList></PlayersList>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import PlayersList from "@/components/PlayersList.vue"; // @ is an alias to /src
+
+@Options({
+  components: {
+    PlayersList,
+  },
+})
+export default class Players extends Vue {}
+</script>
